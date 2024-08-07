824dcceabd436f78b29c085274a8f232066f9f0b
Hek-tor <EmilioCmDz@gmail.com>
2024-08-06 20:45:48 -0600

    Add nodemailer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+const nodemailer = require('nodemailer');
 const app = express();
 
 app.use(bodyParser.json());
@@ -44,13 +45,112 @@ app.get('/products', (req, res) => {
 });
 
 app.post('/invoice', (req, res) => {
-    const userInvoice = req.body;
-    console.log('Datos recibidos del frontend:', userInvoice);
-    res.status(200).json({ message: 'Factura recibida con éxito', data: userInvoice });
+    const { orders, customer, totalPrice } = req.body;
+    const [customerName, customerNumber, customerAddress] = customer;
+    const orderList = orders.join(', ');
+    let emailHTML = htmlTemplate(orderList, customerName, customerNumber, customerAddress, totalPrice);
+    let transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: `${process.env.EMAIL_CLIENT}`,
+        subject: 'NUEVO PEDIDO REALIZADO',
+        html: emailHTML
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.error('Error al enviar correo:', error);
+        }
+        console.log('Correo enviado:', info.response);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
 });
+
+function htmlTemplate(orders, name, number, address, totalPrice) {
+    const emailHTML = `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #f9f9f9;
+            }
+            .container {
+                width: 95%;
+                margin: 20px auto;
+                background-color: #ffffff;
+                border-radius: 8px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                padding: 20px;
+                box-sizing: border-box;
+            }
+            h1 {
+                color: #298779;
+            }
+            .section {
+                margin-bottom: 25px;
+            }
+            .section h2 {
+                color: #F77F00;
+                margin-bottom: 15px;
+            }
+            .section p {
+                margin: 8px 0;
+                color: #666666;
+            }
+            .footer {
+                text-align: center;
+                margin-top: 20px;
+                font-size: 0.9em;
+                color: #999999;
+            }
+            ul {
+                padding-left: 20px;
+            }
+            li {
+                margin-bottom: 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Nuevo pedido</h1>
+            <div class="section">
+                <h2>Detalles del Pedido</h2>
+                <p><strong>${orders}</strong></p>
+            </div>
+            <div class="section">
+                <h2>Detalles del Cliente</h2>
+                <p><strong>Nombre:</strong> ${name}</p>
+                <p><strong>Número telefónico:</strong> ${number}</p>
+                <p><strong>Dirección:</strong> ${address}</p>
+            </div>
+            <div class="section">
+                <h2>Total</h2>
+                <h3><p><strong></strong> ${totalPrice} colones</p></h3>
+            </div>
+            <div class="footer">
+                <p>Este correo se genera de manera automática. No responder.</p>
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+    return emailHTML;
+}
